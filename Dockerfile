c6ff90c85671148d7bf6cb902356139b58c9c23e
Louis Narmour <louis.narmour@colostate.edu>
2024-01-28 00:31:39 +0000

    add coverage tooling

--- Dockerfile
+++ Dockerfile
@@ -19,4 +19,16 @@ run cd /opt && \
     wget https://repo1.maven.org/maven2/junit/junit/4.13.2/junit-4.13.2.jar && \
     wget https://repo1.maven.org/maven2/org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar
 
+# get jacoco libs for test coverage
+run mkdir -p /opt/jacoco && \
+    cd /opt/jacoco && \
+    wget -O dl.zip https://search.maven.org/remotecontent?filepath=org/jacoco/jacoco/0.8.11/jacoco-0.8.11.zip && \
+    unzip dl.zip
+run printf '#!/bin/bash\njava -jar /opt/jacoco/lib/jacococli.jar $@\n' > /usr/local/bin/jacococli && \
+    chmod +x /usr/local/bin/jacococli
+
+# get the code climate report tools
+run wget -O /usr/local/bin/cc-test-reporter https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 && \
+    chmod +x /usr/local/bin/cc-test-reporter
+
 add run-junit-tests.sh /usr/local/bin/org.junit.runner.JUnitCore
--- run-junit-tests.sh
+++ run-junit-tests.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
 if [[ -z $ALPHA_REPO_ROOT ]]; then
-  echo "ALPHA_REPO_ROOT environment variable must be set and pointing to the repository root directory"
-  exit 1
+  ALPHA_REPO_ROOT=/alpha
+  echo "ALPHA_REPO_ROOT not specified, defaulting to '$ALPHA_REPO_ROOT'"
 fi
+if [[ -z $JACOCO_PREFIX ]]; then
+  JACOCO_PREFIX=/opt/jacoco/reports
+  echo "JACOCO_PREFIX not specified, defaulting to '$JACOCO_PREFIX'"
+fi
+
+mkdir -p `basename $ALPHA_REPO_ROOT`
+mkdir -p `basename $JACOCO_PREFIX`
 
 ALPHA_REPO_ROOT=`realpath $ALPHA_REPO_ROOT`
+JACOCO_PREFIX=`realpath $JACOCO_PREFIX`
 
-jars=`echo /opt/eclipse/plugins/*.jar | sed 's~ ~:~g'`
+# git requires that the current user be the owner of the files
+# this occurs when the repo is mounted "into" a container
+git config --global --add safe.directory $ALPHA_REPO_ROOT
+
+if [[ -z $GITHUB_SHA ]]; then
+  REV=$(git rev-parse HEAD)
+else
+  REV=$GITHUB_SHA
+fi
+
 bins=`echo $ALPHA_REPO_ROOT/*/*/bin | sed 's~ ~:~g'`
+jars=`echo /opt/eclipse/plugins/*.jar | sed 's~ ~:~g'`
 pushd $ALPHA_REPO_ROOT/tests/alpha.model.tests/
 
-if [[ -z $1 ]]; then
-  java -cp $bins:$jars org.junit.runner.JUnitCore alpha.model.tests.*
-else
-  java -cp $bins:$jars org.junit.runner.JUnitCore $@
-fi
+JACOCO_DESTFILE=$JACOCO_PREFIX/jacoco-$REV.exec
+java -javaagent:/opt/jacoco/lib/jacocoagent.jar=destfile=$JACOCO_DESTFILE -cp $bins:$jars org.junit.runner.JUnitCore $@
