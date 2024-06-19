c72def524e9e717e4d1ee494ce0b66ae2c48d3c8
Ryan Job <ryanjob42@gmail.com>
2024-06-19 14:38:04 -0600

    Adding a new environment variable to allow different test packages

--- run-junit-tests.sh
+++ run-junit-tests.sh
@@ -4,6 +4,10 @@ if [[ -z $ALPHA_REPO_ROOT ]]; then
   ALPHA_REPO_ROOT=/alpha
   echo "ALPHA_REPO_ROOT not specified, defaulting to '$ALPHA_REPO_ROOT'"
 fi
+if [[ -z $TEST_PACKAGE ]]; then
+  TEST_PACKAGE="alpha.model.tests"
+  echo "TEST_PACKAGE not specified, defaulting to '$TEST_PACKAGE'"
+fi
 if [[ -z $JACOCO_PREFIX ]]; then
   JACOCO_PREFIX=/opt/jacoco/reports
   echo "JACOCO_PREFIX not specified, defaulting to '$JACOCO_PREFIX'"
@@ -27,7 +31,9 @@ fi
 
 bins=`echo $ALPHA_REPO_ROOT/*/*/bin | sed 's~ ~:~g'`
 jars=`echo /opt/eclipse/plugins/*.jar | sed 's~ ~:~g'`
-pushd $ALPHA_REPO_ROOT/tests/alpha.model.tests/
+pushd $ALPHA_REPO_ROOT/tests/$TEST_PACKAGE/
 
-JACOCO_DESTFILE=$JACOCO_PREFIX/jacoco-$REV.exec
+JACOCO_DESTFILE=$JACOCO_PREFIX/jacoco-$REV-$TEST_PACKAGE.exec
 java -javaagent:/opt/jacoco/lib/jacocoagent.jar=destfile=$JACOCO_DESTFILE -cp $bins:$jars org.junit.runner.JUnitCore $@
+
+popd
