3be0d77e644da1689a8cce84660bb6234fb4e9c9
Ryan Job <ryanjob42@users.noreply.github.com>
2024-06-19 15:06:48 -0600

    Update and rename build-image.sh to build-and-push-image.sh
    The script now requires a tag to be put in. After building the image, it will automatically tag it as the latest and push it to GitHub.

---
+++ build-and-push-image.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ -z $1 ]]; then
+  echo "You must enter a tag as the first argument to this script."
+  exit -1
+fi
+
+# Build the image itself and push it to GitHub.
+docker build -t ghcr.io/csu-cs-melange/alpha-language-image:$tag . --no-cache
+docker push ghcr.io/csu-cs-melange/alpha-language-image:$tag
+
+# Tag the image as being the latest and push that to GitHub also.
+docker tag ghcr.io/csu-cs-melange/alpha-language-image:$tag tag ghcr.io/csu-cs-melange/alpha-language-image:latest
+docker push ghcr.io/csu-cs-melange/alpha-language-image:latest
--- build-image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ -z $1 ]]; then
-  tag=latest
-else
-  tag=$1
-fi
-
-docker build -t ghcr.io/csu-cs-melange/alpha-language-image:$tag .
